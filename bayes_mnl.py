bbf8ac544bd61d3ad925ed2ad1c7a60cf6bd83a9
Spencer Aeschliman <spenceraeschliman2026@u.northwestern.edu>
2024-01-16 12:50:27 -0600

    initial commit

---
+++ bayes_mnl.py
@@ -0,0 +1,23 @@
+import numpy as np 
+import pandas as pd 
+import pymc as pm 
+
+# ------------ DATA PREP -------------
+
+prepped_filename = "mt_ce_data"
+raw_filename = "IS1326_ce_data_BIOGEME_updated"
+
+def prep_data(prepped_filename):
+    try:
+        return pd.read_csv(f"bayes_data/{prepped_filename}.csv")
+    except:
+        print("Data file has not been prepared. Importing raw data file...")
+        raw = import_data()
+        # TODO: prepare/clean the data here, if needed
+        print("Cleaned file has been saved do bayes_data directory")
+        raw.to_csv(f"bayes_data/{prepped_filename}.csv")
+
+def import_data(raw_filename):
+    data = pd.read_csv(f"bayes_data/{raw_filename}.csv")
+    return data
+
